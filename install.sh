fd9e883c070d379d036a2d57d24b2c099b084f12
theoboldalex <theoboldalex@gmail.com>
2023-12-19 18:18:16 +0000

    linking term and tmux

--- install.sh
+++ install.sh
@@ -2,6 +2,14 @@
 
 DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# ALACRITTY
+rm -rf $HOME/.config/alacritty
+ln -s $DOTFILES/alacritty $HOME/.config/alacritty
+
+# TMUX
+rm -rf $HOME/.config/tmux
+ln -s $DOTFILES/tmux $HOME/.config/tmux
+
 # NVIM
 rm -rf $HOME/.config/nvim
 ln -s $DOTFILES/nvim $HOME/.config/nvim
