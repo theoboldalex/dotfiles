42f2bc76aacc58a5ee9219e84125398db4bfb373
theoboldalex <theoboldalex@gmail.com>
2024-01-18 21:00:15 +0000

    disable lualine in neotree

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,7 +2,11 @@ return {
     'nvim-lualine/lualine.nvim',
     config = function()
         require('lualine').setup({
-            options = { section_separators = '', component_separators = '' },
+            options = {
+                section_separators = '',
+                component_separators = '',
+                disabled_filetypes = { 'neo-tree' },
+            },
             sections = {
                 lualine_a = {'mode'},
                 lualine_b = {'branch', 'diff', 'diagnostics'},
