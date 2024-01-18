456886565073191d5fb16bbdb1e022b86abe3816
theoboldalex <theoboldalex@gmail.com>
2024-01-18 20:54:16 +0000

    bringing back a simplified lualine

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,16 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+        require('lualine').setup({
+            options = { section_separators = '', component_separators = '' },
+            sections = {
+                lualine_a = {'mode'},
+                lualine_b = {'branch', 'diff', 'diagnostics'},
+                lualine_c = {'filename'},
+                lualine_x = {'filetype'},
+                lualine_y = {'progress'},
+                lualine_z = {'location'}
+            },
+        })
+    end
+}
