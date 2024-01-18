abb63b3f0b00195d224e997dc6d22f8874e0fa37
theoboldalex <theoboldalex@gmail.com>
2024-01-18 20:08:46 +0000

    neo tree fix

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,6 +7,8 @@ return {
              vim.g.nord_italic = false
              vim.g.nord_disable_background = true
              vim.cmd("colorscheme nord")
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
          end,
      }
  }
--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -3,5 +3,26 @@ return {
     branch = "v3.x",
     dependencies = {
         "MunifTanjim/nui.nvim",
-    }
+    },
+    config = function()
+        require("neo-tree").setup({
+            default_component_configs = {
+                git_status = {
+                    symbols = {
+                        -- Change type
+                        added     = "✚",
+                        modified  = "",
+                        deleted   = "✖",
+                        renamed   = "",
+                        -- Status type
+                        untracked = "",
+                        ignored   = "",
+                        unstaged  = "~",
+                        staged    = "",
+                        conflict  = "",
+                    }
+                },
+            }
+        })
+    end
 }
