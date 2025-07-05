7d7e55ba87e82fa434d25f27de899f4b5a614439
theoboldalex <theoboldalex@gmail.com>
2025-07-06 00:10:09 +0100

    reuse the open window if one exists

--- nvim/lua/user/ai.lua
+++ nvim/lua/user/ai.lua
@@ -2,7 +2,7 @@ vim.keymap.set("n", "<C-a><C-i>", function()
     local cwd = vim.loop.cwd()
     local buf = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
     local line = vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win())[1]
-    local mask = "!code %s -g %s:%s"
+    local mask = "!code -r %s -g %s:%s"
 
     vim.cmd(string.format(mask, cwd, buf, line))
 end, { silent = true })
