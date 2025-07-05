157719e385005d4a08bbdea47eb5a0f574cb2292
theoboldalex <theoboldalex@gmail.com>
2025-07-05 23:42:35 +0100

    open vscode in the cwd with current buf and cursor position preserved

--- nvim/lua/user/ai.lua
+++ nvim/lua/user/ai.lua
@@ -1,3 +1,12 @@
+local cmd = {
+    ":!code ",
+    vim.loop.cwd(),
+    " -g ",
+    vim.api.nvim_buf_get_name(0),
+    ":",
+    vim.api.nvim_win_get_cursor(0)[1],
+}
+
 vim.keymap.set("n", "<C-a><C-i>", function()
-    vim.cmd(":!code -g " .. vim.api.nvim_buf_get_name(0) .. ":" .. vim.api.nvim_win_get_cursor(0)[1])
-end)
+    vim.cmd(table.concat(cmd))
+end, { silent = true })
