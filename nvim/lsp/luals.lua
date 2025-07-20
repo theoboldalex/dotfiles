c7f7da1b17f65b5cb749137ecb7086b748ab50ca
theoboldalex <theoboldalex@gmail.com>
2025-07-20 14:46:05 +0100

    stop diagnostic warnings for vim global use in Lua

--- nvim/lsp/luals.lua
+++ nvim/lsp/luals.lua
@@ -1,11 +1,14 @@
 return {
-    cmd = { 'lua-language-server' },
-    filetypes = { 'lua' },
-    root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+    cmd = { "lua-language-server" },
+    filetypes = { "lua" },
+    root_markers = { { ".luarc.json", ".luarc.jsonc" }, ".git" },
     settings = {
         Lua = {
+            diagnostics = {
+                globals = { "vim" }
+            },
             runtime = {
-                version = 'LuaJIT',
+                version = "LuaJIT",
             }
         }
     }
