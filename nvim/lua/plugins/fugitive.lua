390c6a0b38b74e17aae006cc1cdf09ac9e2d3928
Alex Theobold <alex.theobold@sykescottages.co.uk>
2025-07-21 09:25:35 +0100

    adds git pull keybind

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -3,6 +3,7 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>gb", "<cmd>Git blame -w<cr>")
         vim.keymap.set("n", "<leader>gp", "<cmd>Git push<cr>")
+        vim.keymap.set("n", "<leader>gl", "<cmd>Git pull<cr>")
         vim.keymap.set("n", "<leader>gc", ":Git checkout ")
         vim.keymap.set("n", "<leader>gs", ":0G<cr>")
         vim.keymap.set("n", "g[", "<cmd>diffget //2<cr>")
